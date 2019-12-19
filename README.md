c3de2691deb12724a517d986a77baaa8ff690674
d-bryan <dylan.g.bryan@gmail.com>
2019-12-19 05:30:52 -0500

    revisions complete, addding to gh-pages

---
+++ README.md
@@ -0,0 +1,34 @@
+# Responsive Page Build Exercise
+
+## Part 1: HTML + CSS
+
+The goal of this exercise is:
+
+Use the two images below and the assets file to create a mobile first responsive web page. 
+
+Here are the two images:
+[Mobile](https://d13p2xj50zkyqm.cloudfront.net/promos/test/mobile.png)
+[Desktop](https://d13p2xj50zkyqm.cloudfront.net/promos/test/desktop.png)
+
+Here is a document with build assets:
+[Build Assets](https://docs.google.com/document/d/1b8karDU2kxnajjFMKa-8sTW1ujvx51NNcn8WR9V6L58/edit?usp=sharing)
+
+Please note in this exercise we are looking to identify technical areas for training. So, if we’re successful, we’ll have a foundation from which to identify the skills that we should hone together should we decide to move forward.
+
+## Part 2: JavaScript
+
+The goal of this exercise is:
+Add functionality to the mockup above using jquery or plain javaScript.
+Please use comments to describe your javascript logic
+
+Sync up the two checkboxes on the page so both check and uncheck at the same time.
+
+In the blue box  below the words, “TIME LEFT UNTIL THE WEBINAR STARTS...” please create a countdown timer. When the page loads, the timer should begin counting down to 5 days at midnight from then.
+
+Make it so when someone clicks either of the two blue buttons below the checkboxes and the checkboxes are not checked an alert appears asking the user to check the checkbox before proceeding.
+
+When the user clicks either of the two blue buttons below the checkboxes and the checkboxes are checked make a call to the following endpoint:
+
+`https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/`
+
+After receiving a response from this call on button click please parse the response of the call and place the value of `submitok` below the buttons.
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// TODOS 
-// - sync up both of the check boxes so the check and uncheck at the same time ---- DONE
-
-// - In the blue box  below the words, “TIME LEFT UNTIL THE WEBINAR STARTS...” please create a countdown timer. When the page loads, the timer should begin counting down to 5 days at midnight from then. ---- DONE
-
-// - Make it so when someone clicks either of the two blue buttons below the checkboxes and the checkboxes are not checked an alert appears asking the user to check the checkbox before proceeding.  ---- DONE
-
-// - When the user clicks either of the two blue buttons below the checkboxes and the checkboxes are checked make a call to the following endpoint: "https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/" ---- DONE
-
-// - After receiving a response from this call on button click please parse the response of the call and place the value of submitok below the buttons. ---- DONE
-
 // ensure that the webpage has loaded before running any scripts
 $(document).ready(function(){
 
@@ -23,7 +12,7 @@ $(document).ready(function(){
 
     // if the checkboxes are not checked then alert the user to enroll for newsletter
     if ($("input[type='checkbox'").is(':checked') === false) {
-      alert("Please ensure that you have checked the checkboxes to enroll for the newsletter before moving on to the webinar.");
+      alert("Please ensure that you have checked the checkboxes before proceeding to the webinar.");
     }
 
     // make call to API endpoint and set the text on the page
@@ -39,13 +28,15 @@ $(document).ready(function(){
     }
   })
 
+  // add five days to timer start
+  const fiveDays = ((1000 * 60 * 60 * 24) * 5 );
+  // set the webinar start date
+  const webinarDate = new Date().getTime() + fiveDays;
   // set the clock to tick every second
   var x = setInterval(function() {
 
     // set the timer start time 
     var timerStartDate = new Date().getTime();
-    // set the webinar start date
-    var webinarDate = new Date('December 24, 2019 00:00:00').getTime();
 
     // find the difference in time between the two dates
     var timeDifference = webinarDate - timerStartDate;
